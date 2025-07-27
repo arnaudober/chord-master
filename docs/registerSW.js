5a9d202b9809fe7791e810986545ee0f96a596d1
Arnaud Ober <arnaud.ober@icloud.com>
2025-07-27 13:44:42 -0400

    Remove unused bundled script file from the assets directory.

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/jazz-chord-trainer/sw.js', { scope: '/jazz-chord-trainer/' })})}
